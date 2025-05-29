f1f56b00540c32d8b7dc9bffde98effec5ae51e8
Alexander Svyatokho <alexander.svyatokho@gmail.com>
2025-05-29 21:42:36 +0300

    ollama test

--- src/rag.py
+++ src/rag.py
@@ -1,1 +1,17 @@
-print ('hello from rag.py')
+from openai import OpenAI
+
+OLLAMA_API = "http://localhost:11434/v1"
+MODEL = "llama3.2"
+
+messages = [
+    {"role": "user", "content": "Describe some of the business applications of Generative AI"}
+]
+
+ollama_via_openai = OpenAI(base_url=OLLAMA_API, api_key='ollama')
+
+response = ollama_via_openai.chat.completions.create(
+    model=MODEL,
+    messages=messages
+)
+
+print(response.choices[0].message.content)
